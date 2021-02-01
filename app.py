0f878286a18fc53731c336e4d3ef627551dcd950
John Curry <jtc3852@gmail.com>
2021-01-31 20:25:59 -0500

    Step 8, added session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import *
 app = Flask(__name__)
@@ -8,22 +8,27 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 
-responses = []
-
-
 @app.route("/")
 def render_home():
     """Show user the survey title and instructions with a start button"""
     return render_template("home.html", survey=satisfaction_survey)
 
 
+@app.route("/start", methods=["POST"])
+def start_survey():
+    """Redirect user to first question and clear session"""
+    session["responses"] = []
+    return redirect("/questions/0")
+
+
 @app.route("/questions/<int:question_id>")
 def show_question(question_id):
     """Show current question with choices"""
     survey = satisfaction_survey
+    responses = session.get("responses")
     if (responses is None):
         return redirect("/")
-    if (len(responses) >= len(satisfaction_survey.questions)):
+    if (len(responses) >= len(survey.questions)):
         return redirect("/thanks")
     if (len(responses) != question_id):
         flash("Invalid question id", "invalid")
@@ -35,8 +40,13 @@ def show_question(question_id):
 @app.route("/answer", methods=["POST"])
 def handle_answer():
     """Add answer to response list and rediect to next question"""
+
+    # ----> Is there a simpler solution for the following code? Why rebind?
     answer = request.form["answer"]
+    responses = session["responses"]
     responses.append(answer)
+    session["responses"] = responses
+
     if (len(responses) == len(satisfaction_survey.questions)):
         return redirect("/thanks")
     else:
